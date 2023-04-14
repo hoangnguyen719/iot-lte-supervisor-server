6079cdde9b8ca811a55b3a4cf91b87ca5e02fbaa
hoangnguyen <hoang.m.nguyen719@gmail.com>
2023-04-15 00:30:09 +0700

    Create post method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from datetime import datetime
 
 import models
 from database import engine, SessionLocal
@@ -8,6 +10,15 @@ from models import Song, LteSignal
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+class Signal(BaseModel):
+    ts = datetime.now()
+    pcellid: str
+    rsrq: int
+    rsrp: int
+    frequency_band: int
+    dlbw: int
+    ulbw: int
 
 
 def get_db():
@@ -28,5 +39,9 @@ def read_songs(db: Session = Depends(get_db)):
     return db.query(Song).all()
 
 @app.get('/lte/')
-def read_songs(db: Session = Depends(get_db)):
+def read_lte(db: Session = Depends(get_db)):
     return db.query(LteSignal).all()
+
+@app.post('/post_signal/')
+def create_signal(signal: Signal):
+    return signal
