71eded40e3e85dbd134ffa0e0a9638b1dfb79f94
hoangnguyen <hoang.m.nguyen719@gmail.com>
2023-04-16 00:16:00 +0700

    Remove unncessary lines in init_db

--- init_db.py
+++ init_db.py
@@ -1,4 +1,3 @@
-import argparse
 import sqlalchemy as sa
 
 from database import engine, SessionLocal
@@ -6,41 +5,14 @@ from tables import TABLES, TABL_NAMES
 import models
 
 models.Base.metadata.create_all(bind=engine)
-# Get keyword argument
-# parser = argparse.ArgumentParser(description="Initiate tables in the database.")
-# parser.add_argument(
-#     '-d', '--data', choices=['yes', 'no'], required=False, default='no'
-#     )
-# parser.add_argument(
-#     '-t', '--tables'
-#     , choices=TABL_NAMES, default=TABL_NAMES, nargs='+'
-#     , required=False
-#     )
-# args = parser.parse_args()
-# tables = args.tables
-# dummy_data = (args.data=='yes')
-dummy_data = True
 
 db = SessionLocal()
-insp = sa.inspect(engine)
 
 # for name in tables:
 for name in TABL_NAMES:
-    # Create table if not existing
     model = TABLES[name]['model']
-    # if not insp.has_table(name):
-    #     model.__table__.create(engine)
-    #     msg = f'Create {name}.'
-    # else:
-    #     msg = f'{name} already exists.'
-
-    # Adding data if table is empty 
-    if dummy_data:
-        data = TABLES[name]['data']
-        if db.query(model).count() == 0:
-            for row in data:
-                db.add(row)
-
+    data = TABLES[name]['data']
+    if db.query(model).count() == 0:
+        for row in data:
+            db.add(row)
             db.commit()
-    #         msg += ' Add dummy data.'
-    # print(msg)
