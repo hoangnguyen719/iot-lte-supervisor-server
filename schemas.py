72f6a159278cfcfad2282c9076f5b32c000bb2ec
hoangnguyen <hoang.m.nguyen719@gmail.com>
2023-04-15 10:44:56 +0700

    Update schemas

--- schemas.py
+++ schemas.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 class Signal(BaseModel):
     ts = datetime.now()
     pcellid: str
+    mcc: str
+    mnc: str
     rsrq: int
     rsrp: int
     frequency_band: int
